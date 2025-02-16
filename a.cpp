e6ebcd854ad9232c6b93e7b30ae46966c5629b86
Midhun <midhunmohan0312@gmail.com>
2025-02-16 19:38:39 +0530

    new

--- a.cpp
+++ a.cpp
@@ -36,7 +36,31 @@ int main(){
     int t; cin >> t;
     while(t--) {
         int n; cin >> n;
-        
+        vector<int> v(n);
+        for(int i = 0; i<n; ++i) cin >> v[i];
+        sort(v.begin(), v.end());
+        vector<pair<int,int>> rep;
+        int flag = 1;
+        for(int i = 1; i<n; ++i){
+            if(v[i] == rep.back().first) rep.back().second++;
+            if(v[i] == v[i-1]) rep.pb({v[i], 0});
+        }
+        for(int i = 0; i<rep.size(); ++i) {
+            int a = rep[i].first;
+            int freq = rep[i].second;
+            if(freq == 4) {
+                cout << a << " " << a << " " << a <<  " " << a << endl;
+                flag = 0;
+                break;
+            }
+            for(int i = 0; i<n; ++i) {
+                if(freq > 2  && a == v[i]){
+                    if(upper_bound(v.begin() , v.end() , a) != v.end())
+                }
+            }
+        }
+        if(flag)
+        cout << -1 << endl;
     }
 
 
